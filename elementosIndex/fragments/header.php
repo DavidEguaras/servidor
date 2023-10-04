769c1534c1cdb1538d76ab883651c867282fc712
David <dabenegu@gmail.com>
2023-10-04 15:52:45 +0000

    organizacion1

---
+++ elementosIndex/fragments/header.php
@@ -0,0 +1,21 @@
+<nav>
+
+      <section class="logo">
+        <h3>DAVID_BENEITEZ_EGUARAS():</h3>
+        <p>//Web developer</p>
+        <div id="page-wrap"></div>
+      </section>
+
+      <ul class="nav-links">
+        <li><a class="numbers">01.</a><a class="links" href="#">Temas</a></li>
+        <li><a class="numbers">02.</a><a class="links" href="/tareas/tareasTema2/tarea03/indexTarea03.php">Tareas</a></li>
+        <li><a class="numbers">03.</a><a class="links" href="#">Proyectos Personales</a></li>
+      </ul>
+
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+      
+</nav>
---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-compatible" content="ie=edge" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="elementosIndex/css/style.css" />
+    <title>Servidor de David</title>
+  </head>
+
+  <body>
+
+    <header>
+      <?php include("elementosIndex/fragments/header.php"); ?>
+    </header>
+    
+    <div class="box"></div>
+
+    <script src="elementosIndex/js/app.js"></script>
+    <script src="https://kit.fontawesome.com/62ea397d3a.js"></script>
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.esm.js"
+    ></script>
+    <script
+      nomudule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+  </body>
+</html>
