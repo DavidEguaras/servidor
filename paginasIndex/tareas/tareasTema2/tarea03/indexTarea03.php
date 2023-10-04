9b6566ee077785b1b94cbb524fdd285a711d38c3
David <dabenegu@gmail.com>
2023-10-04 16:02:10 +0000

    no funcionan la tarea03

--- paginasIndex/tareas/tareasTema2/tarea03/indexTarea03.php
+++ paginasIndex/tareas/tareasTema2/tarea03/indexTarea03.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Actividades PHP</title>
-    <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/var/www/servidor/elementosIndex/css/style.css">
 </head>
 <body>
-    <?php include 'fragments/header.php'; ?>
+    
+    <header>
+        <?php include '/var/www/servidor/elementosIndex/fragments/header.php';?>
+    </header>
+
     <?php include 'content/pagina1_content.php'; ?>
-    <script src="JS/index.js"></script>
+    <script src="/var/www/servidor/elementosIndex/js/app.js"></script>
 </body>
 </html>
