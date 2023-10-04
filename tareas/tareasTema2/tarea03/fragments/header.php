bda0517d5fa75b00dc28c18baa5be01431922741
David <dabenegu@gmail.com>
2023-10-04 14:35:51 +0000

    estructuraGeneral0.1

--- tareas/tareasTema2/tarea03/fragments/header.php
+++ tareas/tareasTema2/tarea03/fragments/header.php
@@ -1,8 +1,22 @@
-<div class="container">
-    <h1>Actividades PHP</h1>
-    <ul>
+
+<nav>
+
+      <section class="logo">
+        <h3>DAVID_BENEITEZ_EGUARAS():</h3>
+        <p>//Web developer</p>
+        <div id="page-wrap"></div>
+      </section>
+
+      <ul class="nav-links">
         <li><a href="pagina1.php">Ejercicio 1</a></li>
         <li><a href="pagina2.php?variable=42">Ejercicio 2</a></li>
         <li><a href="pagina3.php?ano=2023&mes=10&dia=3">Ejercicio 3</a></li>
-    </ul>
-</div>
+      </ul>
+
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+      
+</nav>
