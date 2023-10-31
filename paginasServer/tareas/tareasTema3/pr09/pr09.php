43daa60d48746f50031cc57311f25c9dec61dd10
David <dabenegu@gmail.com>
2023-10-31 16:59:25 +0000

    31oct

---
+++ paginasServer/tareas/tareasTema3/pr09/pr09.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="validarpr09.php" method="post">
+        <label>Nombre: </label>
+        <input type="text" name="nombre" value="Nombre"><br>
+
+        <label>Apellidos</label>
+        <input type="text" name="nombre" value="Apellidos"><br>
+
+        <label>Contraseña</label>
+        <input type="password" name="password" value="Password"><br>
+
+        <label>Repetir Contraseña</label>
+        <input type="password" name="password2" value="Password2"><br>
+        
+        <label>Fecha</label>
+        <input type="text" name="fecha" value="Fecha"><br>
+
+        <label>Correo</label>
+        <input type="email" name="email" value="Email"><br>
+
+        <label>Imagen de Perfil: </label>
+        <input type="file" name="imagenPerfil" value="ImagenPerfil"><br>
+
+        <input type="submit" value="submit">
+
+    </form>
+</body>
+</html>
