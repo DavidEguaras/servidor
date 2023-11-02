6cb35c82591b2dbcf67afc046060d4b69c868f01
David <dabenegu@gmail.com>
2023-11-02 19:45:02 +0000

    mas

--- paginasServer/temas/tema3/apuntes/2nov/ficheroLineaEsp.php
+++ paginasServer/temas/tema3/apuntes/2nov/ficheroLineaEsp.php
@@ -1,26 +1,28 @@
 <?php
 
-    $tmp = tempnam(".", "tem.txt");
+$tmp = tempnam(".", "temporal.txt");
 
-    if(file_exists('ficheroLineas.txt')){
-        echo "Existe el archivo";
-        if(!$fp = fopen('ficheroLineas.txt', 'a') || !$ft = fopen($tmp, 'w')){
-            echo "Ha habido un problema al abrir el fichero";
-        } else {
-            $texto = "Mi nueva linea\n";
-            $contador = 0;
-            while($leido = fgets($fp, filesize("ficheroLineas.txt")))
-                fputs($ft, $leido, strlen($leido));
-                if($contador == 1){
-                    fputs($ft, $texto, strlen($texto));
-                }
-            fclose($fp);
-            fclose($ft);
-            unlink("ficheroLineas.txt");
-            rename($tmp, "ficheroLineas.txt");
-        }
+if (file_exists('ficheroLineas.txt')) {
+    echo "Existe el archivo";
+    if (!($fp = fopen('ficheroLineas.txt', 'r')) || !($ft = fopen($tmp, 'w'))) {
+        echo "Ha habido un problema al abrir el fichero";
     } else {
-        echo "No existe el archivo";
+        $texto = "Mi nueva línea";
+        $contador = 1;
+        while ($leido = fgets($fp, filesize("ficheroLineas.txt"))) {
+            fputs($ft, $leido, strlen($leido));
+            if ($contador == 1) {
+                fputs($ft, $texto, strlen($texto));
+                fputs($ft, "\n", strlen("\n"));
+            }
+            $contador++;
+        }
+        fclose($fp);
+        fclose($ft);
+        unlink("ficheroLineas.txt");
+        rename($tmp, "ficheroLineas.txt");
     }
-?>
+} else {
+    echo "No existe el archivo";
+}
 ?>
