6cd4c9798e933d9ce9831d1eec660875dcc42254
David <dabenegu@gmail.com>
2024-04-04 17:04:05 +0000

    cambios

--- EntornoServidor/temas/tema4/psql/confBD.php
+++ EntornoServidor/temas/tema4/psql/confBD.php
@@ -1,5 +1,5 @@
 <?
- 
 define('IP', '192.168.7.210');
 define('USER', 'david');
 define('PASS', 'david');
+define('DB_NAME', 'tienda');
---
+++ paginaWeb/API/model/factory.php
@@ -0,0 +1,31 @@
+<?php
+require_once ('../config/configBD.php');
+
+class Factory{
+    protected $connection = null;
+
+    public function __construct(){
+        try{
+            $dsn = 'mysql:host=' . IP . ';dbname=' . DB_NAME;
+            $this->connection = new PDO($dsn, USER, PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            throw new Exception("Could not connect to database: " . $e->getMessage());
+        }
+    }
+
+
+    public function select($query = "", $params = []){
+        try{
+            $stmt = this->executeStatement($query, $params);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }catch(PDOException $e){
+            throw new Exception($e -> getMessage());
+        }
+    }
+}
+
+
+
+?>
