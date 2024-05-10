ad19e587bc2a5bd828fefcfddce632f0704baddc
David <dabenegu@gmail.com>
2024-05-10 14:48:15 +0000

    cambios

--- paginaWeb/API/config/configBD.php
+++ paginaWeb/API/config/configBD.php
@@ -1,6 +1,6 @@
 <?
  
-define('IP', '192.168.7.210');
+define('IP', '192.168.1.100');
 define('USER', 'david');
 define('PASS', 'david');
 define('DB_NAME','tienda');
---
+++ paginaWeb/MVC/core/configurarAPI.php
@@ -0,0 +1,5 @@
+<?php
+
+define("URI_API", "http://192.168.1.100/paginaWeb/API/index.php/");
+
+?>
